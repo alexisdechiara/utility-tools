f02a843a7d1d75162ff77925cf5b9a2928db8e01
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2021-02-19 10:39:44 +0100

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { TextConverterComponent } from './text-converter/text-converter.component';
+import { ValueConverterComponent } from './value-converter/value-converter.component';
+import { GeneratorComponent } from './generator/generator.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextConverterComponent,
+    ValueConverterComponent,
+    GeneratorComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/value-converter/value-converter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-value-converter',
+  templateUrl: './value-converter.component.html',
+  styleUrls: ['./value-converter.component.sass']
+})
+export class ValueConverterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
