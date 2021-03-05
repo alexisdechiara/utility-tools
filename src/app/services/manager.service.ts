a2fc01d2f5d206ec20793db2882060364e6ae5b1
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2021-03-05 11:08:15 +0100

    Add full interface of sidenav

--- src/app/services/manager.service.ts
+++ src/app/services/manager.service.ts
@@ -3,12 +3,76 @@ export class ManagerService{
     {
       id : 'text-converter',
       name : 'Convertisseur de texte',
-      status : false
+      status : false,
+      types : [
+        {
+          id: 'lowercase',
+          name: 'minuscule',
+          status: false,
+        },
+        {
+          id: 'uppercase',
+          name: 'Majuscule',
+          status: false,
+        },
+        {
+          id: 'titlecase',
+          name: 'Titre',
+          status: false,
+        },
+        {
+          id: 'invert',
+          name: 'Inversé',
+          status: false,
+        },
+        {
+          id: 'return',
+          name: 'Retourné',
+          status: false,
+        }
+      ]
     },
     {
       id : 'value-converter',
       name : 'Convertisseur de valeur',
-      status : false
+      status : false,
+      types : [
+        {
+          id: 'volume',
+          name: 'Volume',
+          status: false,
+        },
+        {
+          id: 'distance',
+          name: 'Distance',
+          status: false,
+        },
+        {
+          id: 'weight',
+          name: 'Poids',
+          status: false,
+        },
+        {
+          id: 'speed',
+          name: 'Vitesse',
+          status: false,
+        },
+        {
+          id: 'angle',
+          name: 'Angle',
+          status: false,
+        },
+        {
+          id: 'intensity',
+          name: 'Intensité électrique',
+          status: false,
+        },
+        {
+          id: 'quantity',
+          name: 'Quantité de matière',
+          status: false,
+        },
+      ]
     },
     {
       id : 'generator',
